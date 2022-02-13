59ee2208a6544bf2dbdfdeb22d38a099408e0edb
Peter070757 <petergroeneveld@home.nl>
2022-02-13 15:32:26 +0100

    Huiswerk loops en arrays les 10

--- loops-and-arrays.js
+++ loops-and-arrays.js
@@ -5,6 +5,7 @@
 // ==========================================
 
 const names = ["Henk", "Piet", "Fred", "Joop"];
+console.log(names);
 
 // Verwachtte uitkomsten:
 // Vóór het script zie je de originele waardes:
@@ -12,8 +13,10 @@ const names = ["Henk", "Piet", "Fred", "Joop"];
 // Na jouw script zie je de aangepaste waardes:
 // console.log(names) geeft: ["Henkje", "Pietje", "Fredje", "Joopje"]
 
-
-
+for (let i = 0; i < names.length; i++) {
+    names[i] = names[i] + "je";
+}
+console.log(names);
 
 // ==========================================
 // Opdracht 2
@@ -22,13 +25,23 @@ const names = ["Henk", "Piet", "Fred", "Joop"];
 // ==========================================
 
 const numbers = [2, 4, 5, 29, 38];
+console.log(numbers)
 
 // Verwachtte uitkomsten:
 // Vóór het script zie je de originele waardes:
 // console.log(numbers) geeft: [2, 4, 5, 29, 38];
 // Na jouw script zie je de aangepaste waardes:
-// console.log(numbers) geeft: [ 4, 8, 10, 87, 76 ];
+// console.log(numbers) geeft: [ 4, 8, 15, 87, 76 ];
 
+for(let i = 0; i < numbers.length; i++){
+    if(numbers[i] % 2 === 0){
+        numbers[i] *= 2;
+    }
+    else{
+        numbers[i] *= 3;
+    }
+}
+console.log(numbers)
 
 
 
@@ -40,7 +53,7 @@ const numbers = [2, 4, 5, 29, 38];
 // Zorg ervoor dat dit ook zou werken als de array wel 100 of 200 getallen zou bevatten!
 // ==========================================
 
-const squares = [30, 2, 8, 24, 11];
+const squares = [30, 2, 8, 24, 11]
 
 // Verwachtte uitkomsten:
 // Vóór het script zie je de originele waardes:
@@ -53,3 +66,9 @@ const squares = [30, 2, 8, 24, 11];
 //   'Het volume van 24 is 13824',
 //   'Het volume van 11 is 1331'
 // ]
+
+for (let i = 0; i < squares.length; i++) {
+    squares[i] = "Het volume van " + squares[i] + " is " + squares[i] * squares[i] * squares[i];
+}
+console.log(squares)
+
